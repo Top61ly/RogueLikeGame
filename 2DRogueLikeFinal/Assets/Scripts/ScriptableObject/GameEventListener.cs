4256e36beef5a986f7eb7a060318d25cae30fce1
Top61ly <Liuyaoshini8>
2017-11-30 23:17:59 +0800

    try dependency injection

---
+++ 2DRogueLikeFinal/Assets/Scripts/ScriptableObject/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [Tooltip("注册的观察者")]
+    public GameEvent Event;
+
+    [Tooltip("相应的Unity事件")]
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+}
